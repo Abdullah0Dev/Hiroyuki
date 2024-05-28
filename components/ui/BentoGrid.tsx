309960158ee6ab15b70c32b7cd6cddffed95424a
Abdullah Dev <160670132+Abdullah0Dev@users.noreply.github.com>
2024-05-28 17:40:05 -0400

    Update BentoGrid.tsx

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -3,6 +3,7 @@ import { IoCopyOutline } from "react-icons/io5";
 
 // Also install this npm i --save-dev @types/react-lottie
 import Lottie from "react-lottie";
+import Image from "next/image";
 
 import { cn } from "@/lib/utils";
 
@@ -90,7 +91,7 @@ export const BentoGridItem = ({
       <div className={`${id === 6 && "flex justify-center"} h-full`}>
         <div className="w-full h-full absolute">
           {img && (
-            <img
+            <Image
               src={img}
               alt={img}
               className={cn(imgClassName, "object-cover object-center ")}
@@ -102,7 +103,7 @@ export const BentoGridItem = ({
             } `}
         >
           {spareImg && (
-            <img
+            <Image
               src={spareImg}
               alt={spareImg}
               //   width={220}
