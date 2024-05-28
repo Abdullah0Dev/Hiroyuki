108dd580d44d9325ecbab34394f9d142d6e92168
Abdullah Dev <160670132+Abdullah0Dev@users.noreply.github.com>
2024-05-28 17:14:40 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
 export default withSentryConfig(nextConfig, {
 // For all available options, see:
 // https://github.com/getsentry/sentry-webpack-plugin#options
-
+telemetry: false,
 org: "dev-yhp",
 project: "javascript-nextjs",
 
