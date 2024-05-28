883aa8425367b5787fe7e6c8e3656ebb5dda292e
Abdullah Dev <160670132+Abdullah0Dev@users.noreply.github.com>
2024-05-28 17:33:33 -0400

    Update Experience.tsx

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { workExperience } from "@/data";
 import { Button } from './ui/MovingBorders';
-
+import Image from "next/image";
 const Experience = () => {
   return (
     <div className="py-20 w-full">
@@ -30,7 +30,7 @@ const Experience = () => {
             className="flex-1 text-black dark:text-white border-neutral-200 dark:border-slate-800"
           >
             <div className="flex lg:flex-row flex-col lg:items-center p-3 py-6 md:p-5 lg:p-10 gap-2">
-              <img
+              <Image
                 src={card.thumbnail}
                 alt={card.thumbnail}
                 className="lg:w-32 md:w-20 w-16"
