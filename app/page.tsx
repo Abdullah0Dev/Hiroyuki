c28ecada21be27c778467b540513462f40257f4f
Abdullah Dev <160670132+Abdullah0Dev@users.noreply.github.com>
2024-05-29 15:37:47 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import { navItems } from "@/data";
 // overflow-clip to hide the bottom bar
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative text-white bg-white-100 dark:bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav
         navItems={navItems}
