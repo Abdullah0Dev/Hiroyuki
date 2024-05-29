db204871f16782ac741cc2a811c630f8fee1f219
Abdullah Dev <160670132+Abdullah0Dev@users.noreply.github.com>
2024-05-29 15:47:48 -0400

    Update InfiniteCards.tsx

--- components/ui/InfiniteCards.tsx
+++ components/ui/InfiniteCards.tsx
@@ -3,8 +3,9 @@
 import { testimonials } from "@/data";
 import { cn } from "@/lib/utils";
 import { log } from "console";
-import React, { useEffect, useState } from "react";
 import Image from "next/image";
+import React, { useEffect, useState } from "react";
+
 export const InfiniteMovingCards = ({
   items,
   direction = "left",
@@ -96,16 +97,16 @@ export const InfiniteMovingCards = ({
         {items.map((item, idx) => (
           <li
             //   change md:w-[450px] to md:w-[60vw] , px-8 py-6 to p-16, border-slate-700 to border-slate-800
-            className="w-[90vw] max-w-full relative rounded-2xl border border-b-0
-             flex-shrink-0 border-slate-800 p-5 md:p-16 md:w-[60vw]"
+            className="w-[90vw] dark:bg-black-100 bg-purple  max-w-full relative rounded-2xl border border-b-0
+             flex-shrink-0 border-purple/30 dark:border-slate-800 p-5 md:p-16 md:w-[60vw]"
             style={{
               //   background:
               //     "linear-gradient(180deg, var(--slate-800), var(--slate-900)", //remove this one
               //   add these two
               //   you can generate the color from here https://cssgradient.io/
-              background: "rgb(4,7,29)",
-              backgroundColor:
-                "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
+              // background: "rgb(4,7,29)",
+              // backgroundColor:
+              //   "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
             }}
             // change to idx cuz we have the same name
             key={idx}
@@ -122,7 +123,7 @@ export const InfiniteMovingCards = ({
               <div className="relative z-20 mt-6 flex flex-row items-center">
                 {/* add this div for the profile img */}
                 <div className="me-3">
-                    <img  className="rounded-full w-16 h-16"  src={item.img} alt="profile" />
+                     <img className="rounded-full w-16 h-16"  src={item.img} alt="profile" />
                 </div>
                 <span className="flex flex-col gap-1">
                   {/* change text color, font-normal to font-bold, text-xl */}
@@ -130,7 +131,7 @@ export const InfiniteMovingCards = ({
                     {item.name}
                   </span>
                   {/* change text color */}
-                  <span className=" text-sm leading-[1.6] text-white-200 font-normal">
+                  <span className=" text-sm leading-[1.6] dark:text-white-200 text-white/60 font-normal">
                     {item.title}
                   </span>
                 </span>
