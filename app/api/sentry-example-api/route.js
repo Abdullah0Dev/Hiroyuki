8458a84be7962ac3363d61f06acdec821edda991
Abdullah Dev <160670132+Abdullah0Dev@users.noreply.github.com>
2024-05-28 21:59:18 -0400

    Update route.js

--- app/api/sentry-example-api/route.js
+++ app/api/sentry-example-api/route.js
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
-
-// export const dynamic = "force-dynamic";
-
+ 
+export const dynamic = "force-dynamic";
 // A faulty API route to test Sentry's error monitoring
 export function GET() {
   throw new Error("Sentry Example API Route Error");
