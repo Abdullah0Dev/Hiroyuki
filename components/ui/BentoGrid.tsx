4b69844c911aa4bf7f6b09062e75fb338a47d16f
Abdullah Dev <160670132+Abdullah0Dev@users.noreply.github.com>
2024-05-29 15:39:55 -0400

    Update BentoGrid.tsx

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,15 +1,13 @@
 import { useState } from "react";
 import { IoCopyOutline } from "react-icons/io5";
-
+import {motion} from 'framer-motion'
 // Also install this npm i --save-dev @types/react-lottie
 import Lottie from "react-lottie";
-import Image from "next/image";
 
 import { cn } from "@/lib/utils";
 
-
-import { BackgroundGradientAnimation } from './GradientBg';
-import GridGlobe from './GridGlobe'
+import { BackgroundGradientAnimation } from "./GradientBg";
+import GridGlobe from "./GridGlobe";
 import animationData from "@/data/confetti.json";
 import MagicButton from "../MagicButton";
 
@@ -53,8 +51,32 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
-  const leftLists = ["ReactJS", "Express", "Typescript"];
-  const rightLists = ["VueJS", "NuxtJS", "GraphQL"];
+  const leftLists = [
+    "ReactJS",
+    "Express",
+    "Typescript",
+    "Solidity",
+    "Web3.js",
+    "Node.js",
+    "MongoDB",
+    "Docker",
+    "AWS",
+    "Python"
+  ];
+  
+  const rightLists = [
+    "VueJS",
+    "NuxtJS",
+    "GraphQL",
+    "ReactJS",
+    "Express",
+    "Typescript",
+    "TensorFlow",
+    "Kubernetes",
+    "Google Cloud",
+    "PyTorch"
+  ];
+  
 
   const [copied, setCopied] = useState(false);
 
@@ -80,33 +102,39 @@ export const BentoGridItem = ({
         "row-span-1 dark:bg-[rgb(4,7,29)] relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4",
         className
       )}
-      style={{
-        //   add these two
-        //   you can generate the color from here https://cssgradient.io/
-        //  backgroundColor:
-        //   "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
-      }}
+      style={
+        {
+          //   add these two
+          //   you can generate the color from here https://cssgradient.io/
+          //  backgroundColor:
+          //   "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
+        }
+      }
     >
       {/* add img divs */}
       <div className={`${id === 6 && "flex justify-center"} h-full`}>
         <div className="w-full h-full absolute">
           {img && (
-            <Image
+            <img
               src={img}
               alt={img}
+              width={220}
+              height={220}
               className={cn(imgClassName, "object-cover object-center ")}
             />
           )}
         </div>
         <div
-          className={`absolute right-0 -bottom-5 ${id === 5 && "w-full opacity-80"
-            } `}
+          className={`absolute right-0 -bottom-5 ${
+            id === 5 && "w-full opacity-80"
+          } `}
         >
           {spareImg && (
-            <Image
+            <img
               src={spareImg}
               alt={spareImg}
-              //   width={220}
+              width={220}
+              height={220}
               className="object-cover object-center w-full h-full"
             />
           )}
@@ -125,7 +153,7 @@ export const BentoGridItem = ({
           )}
         >
           {/* change the order of the title and des, font-extralight, remove text-xs text-neutral-600 dark:text-neutral-300 , change the text-color */}
-          <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
+          <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-black/60 dark:text-[#C1C2D3] z-10">
             {description}
           </div>
           {/* add text-3xl max-w-96 , remove text-neutral-600 dark:text-neutral-300*/}
@@ -141,27 +169,28 @@ export const BentoGridItem = ({
 
           {/* Tech stack list div */}
           {id === 3 && (
-            <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+              <div className="flex overflow-y-auto custom-scrollbar  gap-1 lg:gap-5 h-32 w-fit absolute -right-3 lg:-right-2">
               {/* tech stack lists */}
               <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
                 {leftLists.map((item, i) => (
-                  <span
+                  <motion.span 
                     key={i}
                     className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
-                    lg:opacity-100 rounded-lg text-center bg-blue-100 dark:bg-[#10132E]"
+                    lg:opacity-100 rounded-lg text-center text-black-100 dark:text-white bg-blue-100 dark:bg-[#10132E]"
                   >
                     {item}
-                  </span>
+                  </motion.span>
                 ))}
-                <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-blue-100 dark:bg-[#10132E]"></span>
+                <span className="lg:py-4 lg:px-3 py-4 px-3 rounded-lg text-center bg-blue-100 dark:bg-[#10132E]"></span>
               </div>
               <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
-                <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-blue-100 dark:bg-[#10132E]"></span>
+                <span className="lg:py-4 lg:px-3 py-4 px-3 rounded-lg text-center bg-blue-100 dark:bg-[#10132E]"></span>
+                
                 {rightLists.map((item, i) => (
                   <span
                     key={i}
                     className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
-                    lg:opacity-100 rounded-lg text-center bg-blue-100 dark:bg-[#10132E]"
+                    lg:opacity-100 text-black-100 dark:text-white rounded-lg text-center bg-blue-100 dark:bg-[#10132E]"
                   >
                     {item}
                   </span>
@@ -169,6 +198,7 @@ export const BentoGridItem = ({
               </div>
             </div>
           )}
+
           {id === 6 && (
             <div className="mt-5 relative">
               {/* button border magic from tailwind css buttons  */}
@@ -176,10 +206,11 @@ export const BentoGridItem = ({
               {/* remove focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 */}
               {/* add handleCopy() for the copy the text */}
               <div
-                className={`absolute -bottom-5 right-0 ${copied ? "block" : "block"
-                  }`}
+                className={`absolute -bottom-5 right-0 ${
+                  copied ? "block" : "block"
+                }`}
               >
-                {/* <img src="/confetti.gif" alt="confetti" /> */}
+                {/* <Image src="/confetti.gif" alt="confetti" /> */}
                 <Lottie options={defaultOptions} height={200} width={400} />
               </div>
 
@@ -197,3 +228,4 @@ export const BentoGridItem = ({
     </div>
   );
 };
+ 
