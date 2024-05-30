3c3e3103fe2aa1b269becbfb35e1e185cde9046c
Abdullah Dev <160670132+Abdullah0Dev@users.noreply.github.com>
2024-05-30 03:03:15 -0400

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -45,23 +45,18 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <p className="uppercase tracking-widest text-xs text-center dark:text-blue-100 text-blue-400 max-w-80">
-            Dynamic Web Magic with Next.js
+            Dynamic Mobile Magic with ReactNative
           </p>
-
-          {/**
-           *  Link: https://ui.aceternity.com/components/text-generate-effect
-           *
-           *  change md:text-6xl, add more responsive code
-           */}
+ 
           <TextGenerateEffect
-            words="Transforming Concepts into Seamless User Experiences"
+            words="Turning ideas into smooth user experiences."
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
        
             <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
               Hi! I&apos;m{" "}
-              <span className="text-[#d5cce6] cursor-[url(https://cdn-icons-png.freepik.com/512/9135/9135965.png)] font-bold">HIROYUKI</span>, a
-              Python Developer based in Japan.
+              <span className="text-[#d5cce6] cursor-[url(https://cdn-icons-png.freepik.com/512/9135/9135965.png)] font-bold">Abdullah</span>, a
+              Mobile Developer based in Egypt.
             </p>
          
           <a href="#projects">
